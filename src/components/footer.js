b3aa919e07399fa9931b5b99b2d2e7bc64bccef9
Maria Jose Mendez <majomendez88mena@gmail.com>
2021-06-21 20:12:54 -0600

    medium link updated

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,7 +13,7 @@ export default function Footer() {
           </div>
           <div className="flex flex-col lg:flex-row justify-center items-center pb-10">
             <a
-              href="https://marimendez88.dev" 
+              href="https://medium.com/@marimendez88" 
               target="_blank"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
